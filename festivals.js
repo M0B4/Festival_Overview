f82cdeaf52cddda5b191993454a43aed025c2d72
M0B4 <barz.moritz@t-online.de>
2026-04-09 17:55:29 +0200

    dateinamen

--- festivals.js
+++ festivals.js
@@ -1,12 +1,12 @@
 const festivalMetadata = {
-    "wacken_2026.json": {
+    "wacken.json": {
         "date": "29.07. – 01.08.2026",
         "location": "Wacken, Deutschland",
         "website": "https://www.wacken.com",
         "poster": "posters/wacken_2026.jpg", // Pfad zu deinem Bild
         "description": "Das größte Metal-Festival der Welt."
     },
-    "hellfest_2026.json": {
+    "hellfest.json": {
         "date": "18.06. – 21.06.2026",
         "location": "Clisson, Frankreich",
         "website": "https://www.hellfest.fr",
