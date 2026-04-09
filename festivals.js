869e8956047472894f04f01c3b21441c870c8030
M0B4 <barz.moritz@t-online.de>
2026-04-09 20:00:20 +0200

    gmm

--- festivals.js
+++ festivals.js
@@ -14,7 +14,7 @@ const festivalMetadata = {
         "price": "329€",
         "poster": "posters/hellfest_2026.jpg", // Oder eine URL
         "description": ""
-    }
+    };
     "graspop_metal_meeting_2026.json": {
         "date": "18.06. – 21.06.2026",
         "location": "Dessel, Belgien",
