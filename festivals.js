b3d65d9ce9daff6d37bae37fe6706bb66400ecdc
M0B4 <barz.moritz@t-online.de>
2026-04-09 18:11:32 +0200

    preis

--- festivals.js
+++ festivals.js
@@ -4,12 +4,14 @@ const festivalMetadata = {
         "location": "Wacken, Deutschland",
         "website": "https://www.wacken.com",
         "poster": "posters/wacken_2026.jpg", // Pfad zu deinem Bild
+        "price": "333€",
         "description": "Das größte Metal-Festival der Welt."
     },
     "hellfest.json": {
         "date": "18.06. – 21.06.2026",
         "location": "Clisson, Frankreich",
         "website": "https://www.hellfest.fr",
+        "price": "333€",
         "poster": "https://link-zum-hellfest-plakat.jpg", // Oder eine URL
         "description": "Eines der größten Multi-Genre-Metal-Festivals in Europa."
     }
